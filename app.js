8239473b6d138e845c509c7c3545c53e60bf8458
jankovicgd <jankovic.gd@gmail.com>
2017-12-08 01:55:22 +0100

    public

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 var path = require('path');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join( __dirname + '/index.html'));
 });
